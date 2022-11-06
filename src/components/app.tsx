7d70ec77eeb33dfb96c3bd5327c8982997791608
Ryan Henness <84749026+ryan-henness-trimble@users.noreply.github.com>
2022-11-06 15:48:55 -0800

    Init

---
+++ src/components/app.tsx
@@ -0,0 +1,25 @@
+import { h } from 'preact';
+import '../style/index.css';
+import {useState, useEffect} from "preact/hooks";
+
+const App = () => {
+    const [loadingPeriods, setLoadingPeriods] = useState('...');
+
+    useEffect(() => {
+        setInterval(() => {
+            setLoadingPeriods(prevLoadingPeriods => {
+                if (prevLoadingPeriods.length === 3) {
+                    return '.';
+                } else
+                    return prevLoadingPeriods + '.';
+            })
+        }, 800)
+    }, []);
+
+	return <div id="app">
+        <img class="logo" src="assets/logo_color.png" alt="logo" height="170" />
+        <div class="loading">Loading{loadingPeriods}</div>
+    </div>
+};
+
+export default App;
