67ed1b57d9826ed6d3c2914fcb69d01489af144f
Ryan Henness <84749026+ryan-henness-trimble@users.noreply.github.com>
2022-11-07 16:50:29 -0800

    Animation

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,6 +1,7 @@
 import { h } from 'preact';
 import '../style/index.css';
 import {useState, useEffect} from "preact/hooks";
+import { motion } from 'framer-motion';
 
 const App = () => {
     const [loadingPeriods, setLoadingPeriods] = useState('...');
@@ -16,9 +17,36 @@ const App = () => {
         }, 800)
     }, []);
 
+    const container = {
+        hidden: { opacity: 0 },
+        show: {
+            opacity: 1,
+            transition: {
+                staggerChildren: 0.3
+            }
+        }
+    }
+
+    const item = {
+        hidden: {
+            y: -70,
+            opacity: 0
+        },
+        show: { opacity: 1, y: 0 }
+    }
+
 	return <div id="app">
-        <img class="logo" src="assets/logo_color.png" alt="logo" height="110" />
-        <div class="loading">Loading{loadingPeriods}</div>
+        <motion.div
+            style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center'}}
+            variants={container}
+            initial="hidden"
+            animate="show"
+        >
+            <motion.img className="logo" src="assets/logo_color.png" alt="logo" height="110" variants={item} />
+            <motion.div class="loading" variants={item}>
+                Loading{loadingPeriods}
+            </motion.div>
+        </motion.div>
     </div>
 };
 
